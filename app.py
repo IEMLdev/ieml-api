383489e7ec2f2170e01fd3544b56517a9e38df82
Hadrien Titeux <hadwarez@gmail.com>
2016-04-16 13:41:03 -0400

    Added the API route for search and validation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from handlers import SearchTermsHandler, WordGraphValidatorHandler, TextDecompositionHandler, GraphValidatorHandler
+from handlers import *
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,10 +9,16 @@ api = Api(app)
 
 ### This is the api's routing table
 
+# search for a term in the DB
 api.add_resource(SearchTermsHandler, '/api/search_terms')
+# search for a proposition/term in the DB, returns a normalized list of propositions
+api.add_resource(SearchPropositionsHandler, 'api/search_proposition')
+# validate and save a word object, and return its IEML string
 api.add_resource(WordGraphValidatorHandler, '/api/validate_word')
-api.add_resource(TextDecompositionHandler, '/api/decomposition_text')
+# validate and save a proposition (sentence or supersentence) graph, and return its IEML string
 api.add_resource(GraphValidatorHandler, '/api/validate_tree')
+#
+api.add_resource(TextDecompositionHandler, '/api/decomposition_text')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0") # served on the local network
--- handlers/propositions.py
+++ handlers/propositions.py
@@ -1,12 +1,11 @@
 import logging
 
-from ieml.AST.propositions import Term
-from .base import BaseHandler, BaseDataHandler
+from ieml import PropositionsParser
 from ieml.AST import Word, Clause, Sentence, SuperClause, SuperSentence, Morpheme, Term, promote_to
 from ieml.exceptions import InvalidNodeIEMLLevel
-from .exceptions import MissingField
-from ieml import PropositionsParser, USLParser
 from models import PropositionsQueries, DictionnaryQueries
+from .base import BaseHandler, BaseDataHandler
+from .exceptions import MissingField
 
 class SentenceGraph:
 
@@ -96,7 +95,7 @@ class WordGraphValidatorHandler(ValidatorHandler):
         return {"valid" : True, "ieml" : str(word_ast)}
 
 
-class PropositionSearchHandler(BaseHandler):
+class SearchPropositionsHandler(BaseHandler):
     """Search for primitives in the database, for all levels"""
 
     def post(self):
@@ -130,41 +129,3 @@ class PropositionSearchHandler(BaseHandler):
         return result
 
 
-class TextDecompositionHandler(BaseHandler):
-
-    def _entry(self, node):
-        ieml = str(node)
-        elem = DictionnaryQueries().exact_ieml_term_search(ieml)
-        if elem:
-            return {
-                "ieml": ieml,
-                "tags": {
-                    "FR": elem.get("FR"),
-                    "EN": elem.get("EN")
-                }
-            }
-        else:
-            return {
-                "ieml": ieml
-            }
-
-    def _prefix_walker(self, node):
-        result = [self._entry(node)]
-        if not isinstance(node, Term):
-            n_ieml = str(node)
-            for n in node.childs:
-                for child in self._prefix_walker(n):
-                    child["ieml"] = n_ieml + '/' + child["ieml"]
-                    result.append(child)
-        return result
-
-    def post(self):
-        self.reqparse.add_argument("data", required=True, type=str)
-        self.do_request_parsing()
-
-        parser = USLParser()
-        text = parser.parse(self.args['data']);
-        l = [self._prefix_walker(proposition) for proposition in text.propositions]
-        result = [item for sublist in l for item in sublist]
-        print(result)
-        return result
