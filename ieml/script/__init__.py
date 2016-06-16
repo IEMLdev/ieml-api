e4e91aa86f0c5bc2d41045c67544d312d76c2e56
Zack Soliman <zack.soliman@gmail.com>
2016-06-16 14:03:18 -0400

    clean up

--- ieml/script/__init__.py
+++ ieml/script/__init__.py
@@ -1,4 +1,4 @@
 from .script import Script, AdditiveScript, MultiplicativeScript, NullScript
 from .constants import *
 from .tools import RemarkableSibling
-from .tables import *
+from .tables import Variable, Table, build_2d_table
