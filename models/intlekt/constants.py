cf3cc361907bddd9dbf53552fbc57a9f28559eb3
alice <ribaucourtalice@gmail.com>
2016-07-09 01:26:02 +0200

    Create a function to fill a collection with derived terms  (in english, french, spanish, portuguese) of descriptors using the wiktionnary website.

--- models/intlekt/constants.py
+++ models/intlekt/constants.py
@@ -2,3 +2,4 @@ DEMO_DB_NAME = 'intlekt_db'
 COLLECTION_USERS = 'users'
 COLLECTION_USL = 'usl'
 COLLECTION_ENVELOPE = 'envelope'
+LANGUAGES = ['french', 'english', 'spanish', 'portuguese']
--- models/intlekt/usl/usl_connector.py
+++ models/intlekt/usl/usl_connector.py
@@ -1,5 +1,14 @@
+from ieml.AST.propositions import Word, Morpheme
+from ieml.AST.terms import Term
 from models.intlekt.connector import DemoConnector
 from models.intlekt.constants import COLLECTION_USL
+import progressbar
+import pymongo
+from models.terms.terms import TermsConnector
+from urllib.parse import urlencode, quote_plus
+import urllib.request
+from bs4 import BeautifulSoup
+from collections import defaultdict
 
 
 class UslConnector(DemoConnector):
@@ -34,3 +43,66 @@ class UslConnector(DemoConnector):
         return self.usls.find_one({
             '_id': usl if isinstance(usl, str) else str(usl)
         }) is not None
+
+
+    def get_derived_terms_from_wiktionnary(self, descriptor):
+        """
+
+        :param descriptor:
+        :return: a dict language -> set of str representing the drived t$erm in that language
+        """
+        result = {}
+
+        try:
+            response = urllib.request.urlopen('https://en.wiktionary.org/w/index.php?title=' \
+                                              + quote_plus(descriptor.rstrip('\n\r')) + '&printable=yes')
+        except IOError:
+            return result
+        else:
+            html = response.read()
+            soup = BeautifulSoup(html, 'html.parser')
+
+            for language in ("English","French","Portuguese","Spanish"):
+                titles_list = [el.attrs["title"] for el in soup.select('ul li span a[href$="#%s"]'%language) \
+                               if "title" in el.attrs]
+                if titles_list:  # checks if the list is empty
+                    result[language.lower()] = set(titles_list)
+        return result
+
+    #Alice
+    def load_collection(self):
+        self.usls.create_index([('KEYWORDS.DERIVED', pymongo.TEXT)])
+        terms = TermsConnector().get_all_terms()
+        p = progressbar.ProgressBar(max_value=4136)
+        for t in p(terms):
+            w = Word(Morpheme(Term(t['_id'])))
+            w.check()
+
+            if self.check_usl_exists(str(w)): #check si l'usl existe
+                continue
+
+            total_derived_terms = defaultdict(lambda: set())
+            for l in (('EN', 'english'), ('FR', 'french')):
+                for e in t['TAGS'][l[0]].split('|'):
+                    e = e.strip()
+                    total_derived_terms[l[1]].add(e)
+
+                    derived_terms = self.get_derived_terms_from_wiktionnary(e)
+                    if not derived_terms:
+                        continue
+
+                    for key in derived_terms:
+                        total_derived_terms[key] |= derived_terms[key]
+
+            insertion = {
+                '_id': str(w),  #enfin le mot de l usl avec fonction, if isinstance(usl, str) else str(usl) ???
+                'KEYWORDS': [{
+                                 'language': key,
+                                 'DERIVED': list(total_derived_terms[key])
+                             } for key in total_derived_terms]
+            }
+
+            self.usls.insert(insertion)
+
+if __name__ == '__main__':
+    UslConnector().load_collection()
