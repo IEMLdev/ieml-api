b22bd4b3c19b064b761beb1bca9e617a19572525
Louis van Beurden <louis.vanbeurden@gmail.com>
2016-07-27 12:28:37 -0400

    Add the init file (other than in the __init__) for create_app and add a check on the cache creation for authentification.

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from app import create_app, socketio
 import binascii
 from handlers.caching import cache
 from handlers.dictionary.client import PAYLOAD_SALT_CACHE, SECRET_CACHE
-import bcrypt
 import os
+
 
 if __name__ == '__main__':
     app = create_app()
-    cache.set(SECRET_CACHE, binascii.hexlify(os.urandom(24)), timeout=0)
-    cache.set(PAYLOAD_SALT_CACHE, binascii.hexlify(os.urandom(24)), timeout=0)
+
+    if not cache.has(SECRET_CACHE):
+        cache.set(SECRET_CACHE, binascii.hexlify(os.urandom(24)), timeout=0)
+
+    if not cache.has(PAYLOAD_SALT_CACHE):
+        cache.set(PAYLOAD_SALT_CACHE, binascii.hexlify(os.urandom(24)), timeout=0)
+
     socketio.run(app.app, host='0.0.0.0', debug=True)
