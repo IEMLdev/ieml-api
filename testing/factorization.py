3c91e78e56f56e18c2984dda11d41e804a4104c4
Louis van Beurden <louis.vanbeurden@gmail.com>
2016-07-27 21:49:18 -0400

    fix the requirements, duplicate socketio. And update factorisation test.

--- testing/factorization.py
+++ testing/factorization.py
@@ -11,13 +11,7 @@ class FactorizationTest(unittest.TestCase):
         self.terms = TermsConnector()
 
     def test_all_terms(self):
-        exept = {"M:M:.-O:M:.-E:.-+s.y.-'+M:M:.-M:O:.-E:.-+s.y.-'",
-                 *{"%s.-O:M:.-+M:O:.-s.y.-'"%s for s in 'sbtkmnfl'},
-                 "M:M:.-O:M:.-'+M:M:.-M:O:.-'",
-                 "M:M:.-O:M:.-s.y.-'+M:M:.-M:O:.-s.y.-'"}
         term = self.terms.get_all_terms()
         for t in term:
-            if t['_id'] in exept:
-                continue
             self.assertEqual(t['_id'], str(factorize(sc(t['_id']))))
 
