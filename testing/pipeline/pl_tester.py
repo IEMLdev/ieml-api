23b953dddf0c5b9bb448f879286efdfc1f2a9331
hadware <hadwarez@gmail.com>
2016-08-17 07:13:03 -0400

    Finished implementing the paradigmatic experience on filtering. Still needs testing and, well, experimentation.

--- testing/pipeline/pl_tester.py
+++ testing/pipeline/pl_tester.py
@@ -3,6 +3,12 @@ import os
 import pprint
 import sys
 from random import randint, shuffle
+
+from ieml.AST.propositions import Word, Morpheme
+from ieml.AST.terms import Term
+from ieml.operator import script
+from models.terms.terms_queries import get_random_terms
+
 os.chdir(os.path.dirname(sys.argv[0]))
 
 from ieml.AST.tools.random_generation import RandomPropositionGenerator
@@ -16,7 +22,7 @@ class BasePLExperiment:
         self.generator = RandomPropositionGenerator()
         self.results = {}
 
-    def gen_set_and_query(self):
+    def gen_set_and_query(self, **kwargs):
         pass
 
     def run_experiment(self, **kwargs):
@@ -52,7 +58,7 @@ class LinearPLUnitermExperiment(BasicRandomUslsExperiments):
         words = [self.generator.get_random_uniterm_word() for i in range(kwargs["base_nb"])]
         all_usls = []
         for word in words:
-            for i in range(kwargs["rnd_usls"]):
+            for i in range(kwargs["derivative_count"]):
                 new_usl = HyperText(Text([word] +
                                          [self.generator.get_random_uniterm_word()
                                           for i in range(randint(1, 5))]))
@@ -70,7 +76,7 @@ class ConditionalPLMultitermExperiment(BasicRandomUslsExperiments):
         words = [self.generator.get_random_multiterm_word() for i in range(kwargs["base_nb"])]
         all_usls = []
         for word in words:
-            for i in range(kwargs["rnd_usls"]):
+            for i in range(kwargs["derivative_count"]):
                 new_usl = HyperText(Text([word] +
                                          [self.generator.get_random_multiterm_word()
                                           for i in range(randint(1, 5))]))
@@ -84,7 +90,7 @@ class ConditionalPLMultitermExperiment(BasicRandomUslsExperiments):
 class ParadigmaticInclusionUnitermExperiment(BasePLExperiment):
 
     def _get_usl_terms(self, usl):
-        return {word.subst[0] for word in usl.texts[0].children}
+        return {script(word.subst[0].ieml) for word in usl.texts[0].children[0]}
 
     def _get_common_terms_count(self, usls, query_sing_terms):
         output_counts_list = []
@@ -93,6 +99,32 @@ class ParadigmaticInclusionUnitermExperiment(BasePLExperiment):
                 len(query_sing_terms.intersection(self._get_usl_terms(usl)))
             )
         return output_counts_list
+
+    def _get_rnd_elements(self, terms_list, count):
+        return [terms_list[randint(0, len(terms_list) - 1)] for i in range(count)]
+
+    def gen_set_and_query(self, **kwargs):
+        paradigms = [script(term_str) for term_str in get_random_terms(kwargs["base_nb"])]
+        all_cell_uniterms = list(set(Word(Morpheme([Term(str(sing_seq))])) for paradigm in  paradigms
+                             for sing_seq in paradigm.singular_sequences))
+        shuffle(all_cell_uniterms)
+        input_usls = []
+        for paradigm in paradigms:
+            for i in range(kwargs["derivative_count"]):
+                input_usls.append(HyperText(Text(self._get_rnd_elements(all_cell_uniterms, randint(1,3)) +
+                                                 [self.generator.get_random_uniterm_word()
+                                                  for i in range(randint(1, 3))])
+                                            )
+                                  )
+        return input_usls, paradigms[0]
+
+
+    def run_experiment(self, **kwargs):
+        all_usls, query = self.gen_set_and_query(**kwargs)
+        logging.debug("Running with query %s" % str(query))
+        pipeline = LinearPipeline.gen_pipeline_from_query(query)
+        filtered_set = pipeline.filter(USLSet(all_usls), query, 10, [0.1, 0.9])
+        self.process_results(filtered_set.get_usls(), pipeline, query_paradigm=query)
 
     def process_results(self, filtered_usl_pool, pipeline, **kwargs):
         # preparing all kinds of stats on the output
@@ -119,6 +151,6 @@ if __name__ == "__main__":
     results = []
     for i in range(1):
         logging.info("Running experiment %i" % i)
-        experiment.run_experiment(base_nb=20, rnd_usls=15)
+        experiment.run_experiment(base_nb=20, derivative_count=15)
         results.append(experiment.results["query_occurences"])
     logging.info("Average of %i occurrences of query in top 10" % (sum(results)/len(results)))
