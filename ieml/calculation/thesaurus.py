94d9bcc00784662920cf1c0c99e42578cafc2039
Zack Soliman <zack.soliman@gmail.com>
2016-08-17 15:21:31 -0400

    Adds method to rank USLs for either terms or paradigms (it's the same method, the user will give it the appropriate list)

--- ieml/calculation/thesaurus.py
+++ ieml/calculation/thesaurus.py
@@ -1,18 +1,27 @@
 from ieml.AST.terms import Term
 from ieml.AST.propositions import Word, Sentence, SuperSentence
+from ieml.AST.usl import Text, HyperText
 from ieml.script.tables import generate_tables
 from ieml.operator import sc
 from collections import namedtuple, defaultdict
 from ieml.script.constants import AUXILIARY_CLASS, VERB_CLASS, NOUN_CLASS
 from models.terms import TermsConnector
+import bidict
 import numpy as np
 
 ScoreNode = namedtuple('ScoreNode', ['script', 'score'])
-Cache = namedtuple('Cache', ['source_layer', 'source_class'])
+Cache = namedtuple('Cache', ['source_layer', 'source_class', 'source_usl', 'usl_index'])
 ParadigmMetadata = namedtuple('ParadigmMetadata', ['paradigm', 'score', 'nouns', 'auxiliary', 'verb'])
 
 
 def _build_cache(usl_collection):
+
+    def usl_elem_generator(u):
+        if isinstance(u, (Word, Sentence, SuperSentence)):
+            yield u
+        elif isinstance(u, (Text, HyperText)):
+            for c in u.children:
+                yield from usl_elem_generator(c)
 
     # For every term we associate a vector where each coordinate is the number of citations from a given layer
     # First coordinate: SuperSentence
@@ -20,21 +29,29 @@ def _build_cache(usl_collection):
     # Third coordinate: Word
     source_layer = defaultdict(lambda: [0, 0, 0])
     source_class = defaultdict(lambda: [0, 0, 0])
+    # Each term will be mapped to a vector indicating the number of citations from an usl in the usl_collection
+    # each usl is given a coordinate in the usl_index bidict
+    source_usl = defaultdict(lambda: [0 for u in usl_collection])
+    # Every usl is given a coordinate for vector representation
+    usl_index = bidict.bidict({u: i for i, u in enumerate(usl_collection)})
 
-    for usl in usl_collection:
-        for elem in usl.tree_iter():
-            terms = [term for term in usl.tree_iter() if isinstance(term, Term)]
+    for u in usl_collection:
+        for elem in usl_elem_generator(u):
+            terms = [term for term in elem.tree_iter() if isinstance(term, Term)]
             for t in terms:
+                # TODO: Maybe use string representation of terms so that we don't need to worry about Terms/Script
                 source_layer[t.script][coordinate[elem.__class__]] += 1
                 source_class[t.script][elem.grammatical_class] += 1
+                source_usl[t.script][usl_index[u]] += layer_weight[elem.__class__]
 
-    return Cache(source_layer=source_layer, source_class=source_class)
+    return Cache(source_layer=source_layer, source_class=source_class, source_usl=source_usl, usl_index=usl_index)
 
 _cache = None
 
 
 def rank_paradigms(paradigm_list, usl_collection):
     """
+    Paradigms should be of type Script ***NOT TYPE Term***
 
     Parameters
     ----------
@@ -46,6 +63,7 @@ def rank_paradigms(paradigm_list, usl_collection):
 
     """
 
+    # TODO: So check and convert Term types to Script type
     result = []
     global _cache
     if not _cache:
@@ -70,73 +88,29 @@ def rank_paradigms(paradigm_list, usl_collection):
     return sorted(result, key=lambda x: x.score, reversed=True)
 
 
-# 3 Pour chaque paradigme-racine, une liste ordonnée des USLs qui le citent le plus
-def rank_usls(paradigms_list, usl_list):
+def rank_usls(term_list, usl_collection):
     """
-
-    :param paradigms_list: list of the root paradigms we want to analyse
-    :param usl_list: list of the uls (a collection) we want to analyse
-    :return:a dictionary whose keys are the paradigms of paradigms_list
-    and the values : an ordered list of the usls (of usl_list) which most quote the paradigm
-     (first element is usl which quotes the most the paradigm in key), the usls are converted into strings
-     if in usl_list there is the same usl twice, in the returned dictionary this usl will appear only once
-    """
-    paradigm_dico = {p:[] for p in paradigms_list}
-    for p in paradigms_list:
-        p_term = Term(sc(p))
-        usl_dico = {str(usl):0 for usl in usl_list}  # convert usl to string, to use a dictionary
-        for usl in usl_list:
-            term_list = [term for term in usl.tree_iter() if isinstance(term, Term)]
-            for term in term_list:
-                if set(term.script.singular_sequences) <= set(p_term.script.singular_sequences):
-                    usl_dico[str(usl)] += 1
-
-        # we remove the usls which do not quote the paradigm p
-        for usl in usl_list:
-            if str(usl) in usl_dico and usl_dico[str(usl)] == 0:
-                del usl_dico[str(usl)]
-
-        sorted_usls = sorted(usl_dico, key= lambda e: usl_dico[e], reverse=True)  #  this is a list
-        for e in sorted_usls:
-            paradigm_dico[p].append(e)
-
-    return paradigm_dico
-
-
-def rank_usl_terms(term_list, usl_collection):
-    return {term: rank_usl_single_term(term, usl_collection) for term in term_list}
-
-
-def rank_usl_single_term(term, usl_collection):
-    """
-
+    This method assumes the user will correctly giv it a list of root paradigms or of normal terms
     Parameters
+
+    Paradigms should be of type Script ***NOT TYPE Term***
     ----------
-    term
+    term_list: A list of terms, can be ROOT paradigms or plain old terms
     usl_collection
 
     Returns
     -------
-
+    A dictionary that maps for each root paradigm a sorted list of USLs based on the number of citations
     """
 
-    # We will work with a term that is of type Script
-    if isinstance(term, str):
-        term = sc(term)
-    elif isinstance(term, Term):
-        term = term.script
+    # TODO: So check and convert Term types to Script type
 
-    usl_id_map = {usl: i for i, usl in enumerate(usl_collection)}
-    score_count = [0 for i in range(len(usl_collection))]
+    global _cache
+    if not _cache:
+        _cache = _build_cache(usl_collection)
 
-    for i, usl in enumerate(usl_collection):
-        for elem in usl.tree_iter():
-            # I'm checking singular sequence set equality because we can be sure it's the same term object if they
-            # describe the same singular sequence set regardless if it is correctly factorized
-            if isinstance(elem, Term) and elem.script.singular_sequences == term.singular_sequences:
-                score_count[i] += 1
-
-    return sorted(usl_collection, key=lambda e: score_count[usl_id_map[e]], reverse=True)
+    return {term: sorted(usl_collection, key=lambda u: _cache.source_usl[term][_cache.usl_index[u]], reversed=True)
+            for term in term_list}
 
 
 def paradigm_usl_distribution(paradigm, usl_collection):
@@ -149,7 +123,7 @@ def paradigm_usl_distribution(paradigm, usl_collection):
 
     Returns
     -------
-    Distribution of number of terms (contained in 'paradigm') cited overlayed on the paradigms table
+    Distribution of number of terms (contained in 'paradigm') cited overlaid on the paradigms table
     """
 
     TermLocation = namedtuple('TermLocation', ['table', 'coords'])
