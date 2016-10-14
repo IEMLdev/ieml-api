08e52441f8e803afee924999329db116c0ccaef0
Louis van Beurden <louis.vanbeurden@gmail.com>
2016-10-14 18:50:21 -0400

    Remove AST/tools

--- ieml/AST/tools/__init__.py
+++
@@ -1,2 +0,0 @@
-from .tools import demote_once, promote_once, demote_to, promote_to, terms_level_order, null_element, SuperSentenceGraph, SentenceGraph
-from .random_generation import RandomPropositionGenerator
