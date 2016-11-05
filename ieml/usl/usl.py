562c35669ffe0ccda4997570e92d396fcb4118dc
Louis van Beurden <louis.vanbeurden@gmail.com>
2016-11-05 14:36:34 -0400

    Add paths support in usl.

--- ieml/usl/usl.py
+++ ieml/usl/usl.py
@@ -1,8 +1,11 @@
+from ieml.paths.paths import Path
+from ieml.paths.tools import enumerate_paths, resolve
 
 
 class Usl:
     def __init__(self, ieml_object):
         self.ieml_object = ieml_object
+        self._rules = None
 
     def __str__(self):
         return str(self.ieml_object)
@@ -13,3 +16,16 @@ class Usl:
 
     def __hash__(self):
         return hash(self.ieml_object)
+
+    @property
+    def paths(self):
+        if self._rules is None:
+            self._rules = tuple(enumerate_paths(self.ieml_object))
+
+        return self._rules
+
+    def __getitem__(self, item):
+        if isinstance(item, Path):
+            return resolve(self.ieml_object, item)
+
+        raise NotImplemented
