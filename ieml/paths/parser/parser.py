4c6329aca0b2925541144461211f19971a28e5fd
Louis van Beurden <louis.vanbeurden@gmail.com>
2016-11-10 12:57:13 -0500

    Update the paths parser, cleanup old functions.

--- ieml/paths/parser/parser.py
+++ ieml/paths/parser/parser.py
@@ -5,73 +5,8 @@ from ply import yacc
 
 from helpers.metaclasses import Singleton
 from ieml.exceptions import CannotParse
-from ieml.paths.constants import KIND_TO_RANK
 from ieml.paths.parser.lexer import tokens, get_lexer
 from ieml.paths.paths import Coordinate, Path, AdditivePath, MultiplicativePath, ContextPath
-
-
-def _coord(kind, index=None):
-    if kind in KIND_TO_RANK:
-        return Coordinate(kind, index=index)
-    else:
-        #sentence or ss type need context to determine
-        return {
-            'type': 'c',
-            'args': {'kind': kind, 'index': index}
-        }
-
-
-def _plus(children):
-    if any(isinstance(c, Path) for c in children):
-        rank = [c for c in children if isinstance(c, Path)][0].rank
-        return AdditivePath([_resolve(c, rank) for c in children])
-    else:
-        return {
-            'type': '+',
-            'args': children
-        }
-
-
-def _mul(rank_list):
-    rank = None
-    for i, l in enumerate(rank_list):
-        if any(isinstance(c, Path) for c in l):
-            rank = [c for c in l if isinstance(c, Path)][0].rank + i
-            break
-
-    if not rank and len(rank_list) > 1:
-        # we have only tree element
-        if len(rank_list) > 2:
-            raise ValueError('Too many tree structures')
-
-        # supersentence
-        rank = 3
-
-    if rank:
-        return MultiplicativePath([_resolve(c, rank - offset) for offset, l in enumerate(rank_list) for c in l])
-    else:
-        return {
-            'type': '*',
-            'args': rank_list
-        }
-
-
-def _resolve(e, rank):
-    if isinstance(e, Path):
-        if e.rank != rank:
-            raise ValueError('Rank not matching, invalid synthax')
-        return e
-
-    if e['type'] == 'c':
-        return Coordinate(**{**e['args'], 'rank': rank})
-
-    if e['type'] == '+':
-        return AdditivePath([_resolve(c, rank) for c in e['args']])
-
-    if e['type'] == '*':
-        return MultiplicativePath([_resolve(c, rank - offset) for offset, l in enumerate(e['args']) for c in l])
-
-    raise ValueError('Invalid argument')
 
 
 class PathParser(metaclass=Singleton):
@@ -92,9 +27,6 @@ class PathParser(metaclass=Singleton):
         self.parser.parse(s, lexer=self.lexer, debug=False)
 
         if self.root is not None:
-            if not isinstance(self.root, Path):
-                self.root = _resolve(self.root, 2)
-
             if len(self.root.children) == 1:
                 self.root = self.root.children[0]
 
