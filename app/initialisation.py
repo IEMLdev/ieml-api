dbd3e37b661c6498e29832e7d2c2cdd2f5f8f972
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-05-31 16:05:22 -0400

    Fix the unable to create script/parser.pickle and add the all the api.

--- app/initialisation.py
+++ app/initialisation.py
@@ -1,7 +1,7 @@
 from flask_socketio import SocketIO
 import multiprocessing
 from flask_compress import Compress
-
+import os
 socketio = SocketIO()
 
 def create_app():
@@ -16,12 +16,16 @@ def create_app():
     # the best option based on installed packages.
     async_mode = None
 
+    if not os.path.isdir('parser'):
+        os.mkdir('parser')
 
     ## init the usl db for dev
     # init_dev_db()
 
     app = connexion.App(__name__, specification_dir='../api-doc/')
     app.add_api('dictionary_editor.yaml')
+    app.add_api('rest_api.yaml')
+
     app.after_request = lambda *args: None # a dirty hack so flask_cors doesn't screw up
     cors = CORS(app.app, resources={r"/*": {"origins": "*"}})
     app.app.secret_key = 'ZLX9PUQULLAKKLWDI1B9CDZ34H1LIGCW7CA3OVJYWLWF23UW80ONS0REZQAKJKKSFVPIF037VGIXIVE6AYN5AJJRONF2TFKMLLZM'
