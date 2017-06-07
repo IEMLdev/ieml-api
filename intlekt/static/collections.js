81390cbc88b73d7a057024e9612783528089a041
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-06-07 12:49:24 -0400

    Display a collection

--- intlekt/static/collections.js
+++ intlekt/static/collections.js
@@ -1,24 +1,65 @@
 $(function() {
     var API_ROOT = 'http://127.0.0.1:5000/';
 
-    function displayCollection(collection) {
-        $('#collections ul').append(
-            '<li data-id="' + collection.id + '">' +
-            collection.title +
-            ' (' + collection.authors.join(', ') + ')' +
-            '</li>'
-        );
+    function authorsToStr(authors) {
+        return authors.join(', ');
     }
 
-    function createCollection(title, authors) {
+    function authorsToArray(authors) {
+        return authors.split(',').map(function(s) { return s.trim(); });
+    }
+
+    function showCollection(collection) {
+        $('#edit-collection-form input[name="title"]').val(collection.title);
+        $('#edit-collection-form input[name="authors"]').val(authorsToStr(collection.authors));
+        $('#collection-created-on span').html(collection.created_on);
+        $('#collection-updated-on span').html(collection.updated_on);
+
+        $('#collection').show();
+        $('#collection').data('id', collection.id);
+    }
+
+    function writeCollectionToList(collection) {
+        var li = $('#collections ul li[data-id="' + collection.id + '"]');
+        var html = collection.title;
+
+        if(li.length) {
+            li.get(0).innerHTML = html;
+            return;
+        }
+
+        li = document.createElement('li');
+        li.setAttribute('data-id', collection.id);
+        li.innerHTML = html;
+
+        $(li).click(function() {
+            var id = $(this).data('id');
+            $.ajax({
+                url: API_ROOT + 'collections/' + id,
+            })
+            .done(function(data) {
+                showCollection(data);
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                $('#messages').html('Unable to read collection, sorry. Message: ' + jqXHR.responseText);
+            });
+        });
+
+        $('#collections ul').append(li);
+    }
+
+    function writeCollection(id, title, authors) {
+        id = (id == null) ? '' : id;
+
         $.ajax({
-            url: API_ROOT + 'collections/',
-            method: 'POST',
-            data: JSON.stringify({title: title, authors: authors}),
+            url: API_ROOT + 'collections/' + id + '/',
+            method: id ? 'PUT' : 'POST',
+            data: JSON.stringify({title: title, authors: authors, updated_on: null}),
             contentType: 'application/json'
         })
         .done(function(data) {
-            displayCollection(data);
+            writeCollectionToList(data);
+            showCollection(data);
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
             $('#messages').html('Unable to create collection, sorry. Message: ' + jqXHR.responseText);
@@ -32,10 +73,19 @@ $(function() {
         e.preventDefault();
 
         var title = createCollectionForm.find('input[name="title"]').val().trim();
-        var authors = createCollectionForm.find('input[name="authors"]').val();
-        authors = authors.split(',').map(function(s) { return s.trim(); });
+        var authors = authorsToArray(createCollectionForm.find('input[name="authors"]').val());
 
-        createCollection(title, authors);
+        writeCollection(null, title, authors);
+    });
+
+    var editCollectionForm = $('#edit-collection-form');
+    editCollectionForm.submit(function(e) {
+        e.preventDefault();
+    
+        var title = editCollectionForm.find('input[name="title"]').val().trim();
+        var authors = authorsToArray(editCollectionForm.find('input[name="authors"]').val());
+        
+        writeCollection($('#collection').data('id'), title, authors);
     });
 
     // Init
@@ -45,7 +95,7 @@ $(function() {
     })
     .done(function(data) {
         for(var i in data.data) {
-            displayCollection(data.data[i]);
+            writeCollectionToList(data.data[i]);
         }
         $('#messages').html('');
         $('#main').show();
