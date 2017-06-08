624de87db901ae655389d48454b53dbf86049947
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-06-08 18:06:17 -0400

    Split js into two files

---
+++ intlekt/django/intlekt/collections_/static/collections_/api.js
@@ -0,0 +1,68 @@
+var API = function(API_ROOT) {
+    var module = {};
+
+    module.listCollections = function(success, error) {
+        $.ajax({
+            url: API_ROOT + 'collections/',
+        })
+        .done(success)
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            error(errorThrown); 
+        });
+    };
+
+    module.getCollection = function(id, success, error) {
+        $.ajax({
+            url: API_ROOT + 'collections/' + id + '/',
+        })
+        .done(success)
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            error(errorThrown); 
+        });
+    };
+
+    module.createCollection = function(collection, success, error) {
+        $.ajax({
+            url: API_ROOT + 'collections/',
+            method: 'POST',
+            data: JSON.stringify(collection),
+            contentType: 'application/json'
+        })
+        .done(success)
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            error(errorThrown);
+        });
+    };
+    
+    module.updateCollection = function(collection, success, error) {
+        $.ajax({
+            url: API_ROOT + 'collections/' + collection.id + '/',
+            method: 'PUT',
+            data: JSON.stringify(collection),
+            contentType: 'application/json'
+        })
+        .done(success)
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            error(errorThrown);
+        });
+    };
+    
+    module.createDocument = function(doc, collection, success, error) {
+        $.ajax({
+            url: API_ROOT + 'documents/',
+            method: 'POST',
+            data: JSON.stringify(doc),
+            contentType: 'application/json'
+        })
+        .done(function(doc) {
+            module.updateCollection(collection, function(col) {
+                success(doc);
+            }, error);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            error(errorThrown);
+        });
+    };
+
+    return module;
+};
--- intlekt/django/intlekt/collections_/static/collections_/ui.js
+++ intlekt/django/intlekt/collections_/static/collections_/ui.js
@@ -1,7 +1,11 @@
 $(function() {
     var API_ROOT = 'http://127.0.0.1:8000/collections/';
-    var currentCollection = null;
+    var api = API(API_ROOT);
 
+    var collections = [];
+    var documents = [];
+    var currentCollection = null;
+    
     function authorsToStr(authors) {
         return authors.join(', ');
     }
@@ -16,7 +20,34 @@ $(function() {
         return authorsToArray(tags);
     }
 
-    function showCollection(collection) {
+    function renderCollectionList() {
+        var list = $('#collection-list');
+        list.empty();
+        
+        var li;
+
+        for(let collection of collections) {
+            li = document.createElement('li');
+            li.setAttribute('data-id', collection.id);
+            li.innerHTML = collection.title;
+
+            $(li).click(function() {
+                var id = $(this).data('id');
+
+                api.getCollection(id, renderCollection, function(err) {
+                    displayError('Unable to read collection: ' + err); 
+                });
+            });
+
+            list.append(li);
+        }
+    }
+
+    function renderCollection(collection) {
+        currentCollection = collection;
+
+        if(collection == null) return;
+
         $('#edit-collection-form input[name="title"]').val(collection.title);
         $('#edit-collection-form input[name="authors"]').val(authorsToStr(collection.authors));
         $('#collection-created-on span').html(collection.created_on);
@@ -39,77 +70,17 @@ $(function() {
         $('#collection').data('id', collection.id);
     }
 
-    function writeCollectionToList(collection) {
-        var li = $('#collections ul li[data-id="' + collection.id + '"]');
-        var html = collection.title;
-
-        if(li.length) {
-            li.get(0).innerHTML = html;
-            return;
-        }
-
-        li = document.createElement('li');
-        li.setAttribute('data-id', collection.id);
-        li.innerHTML = html;
-
-        $(li).click(function() {
-            var id = $(this).data('id');
-            $.ajax({
-                url: API_ROOT + 'collections/' + id + '/',
-            })
-            .done(function(data) {
-                currentCollection = data;
-                showCollection(data);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                $('#messages').html('Unable to read collection, sorry. Message: ' + jqXHR.responseText);
-            });
-        });
-
-        $('#collections ul').append(li);
+    function collectionUpdated() {
+        renderCollectionList();
+        renderCollection(currentCollection);
     }
 
-    function writeCollection(data, id) {
-        id = (id == null) ? '' : id;
-        data.updated_on = null; // For Python to store the current date
-
-        $.ajax({
-            url: API_ROOT + 'collections/' + id + (id ? '/' : ''),
-            method: id ? 'PUT' : 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        })
-        .done(function(data) {
-            writeCollectionToList(data);
-            showCollection(data);
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            $('#messages').html('Unable to create collection, sorry. Message: ' + jqXHR.responseText);
-        });
+    function displayMessage(msg) {
+        $('#messages').html(msg);
     }
+
+    displayError = displayMessage;
     
-    function writeDocument(data, id, collectionId) {
-        id = (id == null) ? '' : id;
-
-        $.ajax({
-            url: API_ROOT + 'documents/' + id + (id ? '/' : ''),
-            method: id ? 'PUT' : 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        })
-        .done(function(data) {
-            if(currentCollection.documents.indexOf(data.id) == -1) {
-                currentCollection.documents.push(data.id);
-            }
-            writeCollection(currentCollection, currentCollection.id);
-        })
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            $('#messages').html('Unable to create document, sorry. Message: ' + jqXHR.responseText);
-        });
-    }
-
-    // Events
-
     var createCollectionForm = $('#create-collection form');
     createCollectionForm.submit(function(e) {
         e.preventDefault();
@@ -118,20 +89,44 @@ $(function() {
         data.title = createCollectionForm.find('input[name="title"]').val().trim();
         data.authors = authorsToArray(createCollectionForm.find('input[name="authors"]').val());
 
-        writeCollection(data, null);
-    });
+        api.createCollection(data, function(data) {
+            collections.push(data);
+            currentCollection = data;
 
+            collectionUpdated();
+        }, function(err) {
+            displayError('Unable to create collection: ' + err);
+        });
+    });
+    
     var editCollectionForm = $('#edit-collection-form');
     editCollectionForm.submit(function(e) {
         e.preventDefault();
         var data = {};
     
+        data.id = currentCollection.id;
         data.title = editCollectionForm.find('input[name="title"]').val().trim();
         data.authors = authorsToArray(editCollectionForm.find('input[name="authors"]').val());
-        
-        writeCollection(data, $('#collection').data('id'));
+
+        api.updateCollection(data, function(data) {
+            var collection;
+
+            for(let i in collections) {
+                collection = collections[i];
+                if(collection.id == data.id) {
+                    collections[i] = data;
+                    currentCollection = data;
+                    break;
+                }
+            }
+
+            collectionUpdated();
+        }, function(err) {
+            displayError('Unable to update collection: ' + err);
+        });
+
     });
-    
+
     var createDocumentForm = $('#create-document-form');
     createDocumentForm.submit(function(e) {
         e.preventDefault();
@@ -151,22 +146,24 @@ $(function() {
         if(!data.usl) data.usl = null;
         if(!data.image) data.image = null;
 
-        writeDocument(data, null, $('#collection').data('id'));
+
+        api.createDocument(data, currentCollection, function(doc) {
+            currentCollection.documents.push(doc.id);
+            documents[doc.id] = doc;
+            collectionUpdated();
+        }, function(err) {
+            displayError('Unable to create document: ' + err);
+        });
     });
 
-    // Init
 
-    $.ajax({
-        url: API_ROOT + 'collections/',
-    })
-    .done(function(data) {
-        for(var i in data) {
-            writeCollectionToList(data[i]);
-        }
+    api.listCollections(function(data) {
+        collections = data;
+        collectionUpdated();
+
         $('#messages').html('');
         $('#main').show();
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        $('#messages').html('Unable to load collections, sorry. Message: ' + errorThrown);
+    }, function(err) {
+        displayError('Unable to load collections: ' + err); 
     });
 });
