7bbcd917d85454ca608c519939d3f402e7793b5a
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-06-12 15:49:36 -0400

    Add default date for collected document
    When users collect a document manually, we want the collection date to be the current one.

--- intlekt/django/intlekt/collections_/models/collected_document.py
+++ intlekt/django/intlekt/collections_/models/collected_document.py
@@ -1,10 +1,11 @@
+from django.utils import timezone
 from django_mongoengine import EmbeddedDocument, fields
 
 from .tag import USLField
 
 
 class CollectedDocument(EmbeddedDocument):
-    collected_on = fields.DateTimeField(required=True,)
+    collected_on = fields.DateTimeField(default=timezone.now, blank=True,)
     usl = USLField(blank=True, null=True, required=False,)
     tags = fields.ListField(fields.StringField(), required=False, default=list, blank=True,)
     image = fields.URLField(verify_exists=True, null=True, required=False, blank=True,)
