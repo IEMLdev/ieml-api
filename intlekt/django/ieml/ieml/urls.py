834d21a10dfe957116fbd3ce458ff8d57ff04f28
Vincent Lefoulon <vincent.lefoulon@free.fr>
2017-06-19 11:06:36 -0400

    Rename project

--- intlekt/django/ieml/ieml/urls.py
+++ intlekt/django/ieml/ieml/urls.py
@@ -1,4 +1,4 @@
-"""intlekt URL Configuration
+"""ieml URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
