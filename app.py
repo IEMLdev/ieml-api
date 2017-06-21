5ca42b1812c44305db50e6365b38412f37567157
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-06-21 13:28:58 -0400

    Add api port in config.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from app import create_app, socketio
 import binascii
+
+from config import API_PORT
 from handlers.caching import cache
 from handlers.dictionary.client import PAYLOAD_SALT_CACHE, SECRET_CACHE
 import os
@@ -14,4 +16,4 @@ if __name__ == '__main__':
     if not cache.has(PAYLOAD_SALT_CACHE):
         cache.set(PAYLOAD_SALT_CACHE, binascii.hexlify(os.urandom(24)), timeout=0)
 
-    socketio.run(app.app, host='0.0.0.0', debug=True)
+    socketio.run(app.app, host='0.0.0.0', port=API_PORT, debug=True)
