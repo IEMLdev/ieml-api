25a75a96c90e27eb7e0b370b33975449479a5bd1
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-07-06 16:15:12 -0400

    Add singledispatch on term

--- ieml/dictionary/tools.py
+++ ieml/dictionary/tools.py
@@ -1,4 +1,4 @@
-from ..exceptions import TermNotFoundInDictionary
+from functools import singledispatch
 from .version import DictionaryVersion
 from .script import Script
 from .terms import Term
@@ -6,25 +6,27 @@ from .dictionary import Dictionary
 
 
 def term(arg, dictionary=None):
-    if isinstance(arg, Term):
-        return arg
-
     if not isinstance(dictionary, Dictionary):
         if isinstance(dictionary, (str, DictionaryVersion)):
             dictionary = Dictionary(dictionary)
         else:
             dictionary = Dictionary()
 
-    if isinstance(arg, int):
-        return dictionary.index[arg]
+    return _term(arg, dictionary)
 
-    if isinstance(arg, str):
-        if arg[0] == '[' and arg[-1] == ']':
-            arg = arg[1:-1]
 
-    if isinstance(arg, Script) or isinstance(arg, str):
-        if arg in dictionary:
-            return dictionary.terms[arg]
+@singledispatch
+def _term(arg, dictionary):
+    raise NotImplemented
 
-    raise TermNotFoundInDictionary(arg, dictionary)
+_term.register(Term, lambda arg, dictionary: arg)
+_term.register(int, lambda arg, dictionary: dictionary.index[arg])
+_term.register(Script, lambda arg, dictionary: dictionary.terms[arg])
+
+
+@_term.register(str)
+def _term_str(arg, dictionary=None):
+    if arg[0] == '[' and arg[-1] == ']':
+        arg = arg[1:-1]
+    return dictionary.terms[arg]
 
