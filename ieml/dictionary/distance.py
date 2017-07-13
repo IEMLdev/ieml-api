9b5b80a4b3a78e409f47de90a6e77d70b37bd7bc
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-07-13 10:54:55 -0400

    Update distance with pack terms

--- ieml/dictionary/distance.py
+++ ieml/dictionary/distance.py
@@ -1,5 +1,5 @@
 import os
-from itertools import combinations
+from itertools import combinations, product, groupby
 
 import numpy as np
 
@@ -24,41 +24,50 @@ def _sam_to_float(reltype):
     return res
 
 
+
+
 def relation_value(reltype, max_rank=None):
+
     if reltype == 'associated':
-        return C * 1. / 4
+        return C * 1. / 4, RELATIONS_TYPES[reltype]
     elif reltype == 'opposed':
-        return C * 1. / 2
+        return C * 1. / 2, RELATIONS_TYPES[reltype]
     elif reltype == 'crossed':
-        return C * 3. / 4
+        return C * 3. / 4, RELATIONS_TYPES[reltype]
     elif reltype == 'twin':
-        return C
+        return C, RELATIONS_TYPES[reltype]
     elif reltype.startswith('table_'):
         i = int(reltype[6:7])
         # rank
-        return C + k + (1 - 2*k - C) * (max_rank - i) / max_rank
+        # available slot: 2, 4
+        ratio = float(max_rank - i) / max_rank
+        slot = 2 if ratio < 0.5 else 4
+        return C + k + (1 - 2*k - C) * ratio, (slot, i)
     else:
         #sam
         if len(reltype) == 1:
-            return C + k * _sam_to_float(reltype)
+            return C + k * _sam_to_float(reltype), RELATIONS_TYPES[reltype]
         elif len(reltype) == 2:
-            return (1 + C - k) / 2 + k * _sam_to_float(reltype)
+            return (1 + C - k) / 2 + k * _sam_to_float(reltype), RELATIONS_TYPES[reltype]
         elif len(reltype) == 3:
-            return 1 - k + k * _sam_to_float(reltype)
+            return 1 - k + k * _sam_to_float(reltype), RELATIONS_TYPES[reltype]
         else:
             ValueError("Invalid relation %s"%str(reltype))
 
-RELATION_ORDER = {
-    'associated': 0,
-    'opposed': 1,
-    'crossed': 2,
-    'twin': 3,
-    'table_5': 4,
-    'table_4': 5,
-    'table_3': 6,
-    'table_2': 7,
-    'table_1': 8,
-    'table_0': 9
+
+
+RELATIONS_TYPES = {
+    'associated': (0, 1),
+    'opposed': (0, 2),
+    'crossed': (0, 3),
+    'twin': (0, 4),
+    'table_5': (4, 0),
+    'table_4': (4, 1),
+    'table_3': (4, 2),
+    'table_2': (4, 3),
+    'table_1': (4, 4),
+    'table_0': (4, 5),
+    **{''.join(s): (1 + 2*(i-1), j) for i in range(1, 4) for j, s in enumerate(product('sam', repeat=i))}
 }
 
 
@@ -67,16 +76,21 @@ def _enumerate_ancestors(t, prefix=''):
         for t1 in v:
             yield (prefix + k, t1)
 
-            if len(prefix) < 3:
+            if len(prefix) < 2:
                 yield from _enumerate_ancestors(t1, prefix=prefix + k)
 
 
 def build_matrix(version):
     d = Dictionary(version)
-    relation_matrix = np.ones((len(d),) * 2, dtype=np.float32)
+    relation_matrix = np.empty(shape=(len(d),) * 2,
+                            dtype=[('distance', 'f4'),('slot', 'i4', (2,)), ('reltype', 'S16')])
+
+    relation_matrix['reltype'] = 'none'
+    relation_matrix['slot'] = (0, 0)
+    relation_matrix['distance'] = 1.0
 
     for i in range(len(d)):
-        relation_matrix[i, i] = 0.0
+        relation_matrix[i, i] = (0.0, (0, 0), 'none')
 
     for root in d.roots:
         max_rank = max(t.rank for t in root.relations.contains if len(t) != 1) + 1
@@ -84,18 +98,18 @@ def build_matrix(version):
         for t0, t1 in combinations(root.relations.contains, 2):
 
             reltype = min({r for r in t0.relations.to(t1) if r not in ('contained', 'contains')},
-                          key=lambda r: RELATION_ORDER[r])
+                          key=lambda r: RELATIONS_TYPES[r])
 
-            v = relation_value(reltype, max_rank=max_rank)
-            relation_matrix[t0.index, t1.index] = v
-            relation_matrix[t1.index, t0.index] = v
+            v, order = relation_value(reltype, max_rank=max_rank)
+            relation_matrix[t0.index, t1.index] = (v, order, reltype)
+            relation_matrix[t1.index, t0.index] = (v, order, reltype)
 
     for layer in d.layers:
         for t0 in layer:
             for prefix, t1 in _enumerate_ancestors(t0):
-                v = relation_value(prefix)
-                relation_matrix[t0.index, t1.index] = v
-                relation_matrix[t1.index, t0.index] = v
+                v, order = relation_value(prefix)
+                relation_matrix[t0.index, t1.index] = (v, order, prefix)
+                relation_matrix[t1.index, t0.index] = (v, order, prefix)
 
     return relation_matrix
 
@@ -108,9 +122,18 @@ def distance_matrix(version):
         np.save('/tmp/cache_relations_%s.npy'%str(version), mat)
         return mat
 
+
 def default_metric(dictionary_version):
     mat = distance_matrix(dictionary_version)
-    return lambda t0, t1: mat[t0.index, t1.index]
+    return lambda t0, t1: mat[t0.index, t1.index][0]
+
+
+def distance_pack(t, matrix):
+    res = [(*matrix[t.index, i], i) for i in np.where(matrix['reltype'][t.index, :] != b'none')[0]]
+    res = sorted(res, key=lambda t: list(t[1]))
+
+    return groupby(res, key=lambda t: t[2])
+
 
 def rank_from_term(term, metric, nb_terms=30):
     res = sorted([(metric(term, t), t) for t in term.dictionary])[:nb_terms]
