b7a06ec14256c5dbd50ceb00c7171455c445d715
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-08-01 16:51:27 -0400

    Move the metaclasses package in commons

--- ieml/commons.py
+++ ieml/commons.py
@@ -1,4 +1,3 @@
-from .exceptions import InvalidIEMLObjectArgument
 
 
 class cached_property:
@@ -10,6 +9,7 @@ class cached_property:
         attr = self._factory(instance)
         setattr(instance, self._attr_name, attr)
         return attr
+
 
 class TreeStructure:
     def __init__(self):
@@ -44,90 +44,20 @@ class TreeStructure:
             yield from c.tree_iter()
 
 
-class IEMLType(type):
-    """This metaclass enables the comparison of class times, such as (Sentence > Word) == True"""
-
-    def __init__(cls, name, bases, dct):
-        child_list = ['Term', 'Morpheme', 'Word', 'Clause', 'Sentence',
-                     'SuperClause', 'SuperSentence', 'Text', 'Hyperlink', 'Hypertext']
-        if name in child_list:
-            cls.__rank = child_list.index(name) + 1
-        else:
-            cls.__rank = 0
-
-        super(IEMLType, cls).__init__(name, bases, dct)
-
-    def __hash__(self):
-        return self.__rank
-
-    def __eq__(self, other):
-        if isinstance(other, IEMLType):
-            return self.__rank == other.__rank
-        else:
-            return False
-
-    def __ne__(self, other):
-        return not IEMLType.__eq__(self, other)
-
-    def __gt__(self, other):
-        return IEMLType.__ge__(self, other) and self != other
-
-    def __le__(self, other):
-        return IEMLType.__lt__(self, other) and self == other
-
-    def __ge__(self, other):
-        return not IEMLType.__lt__(self, other)
-
-    def __lt__(self, other):
-        return self.__rank < other.__rank
-
-    def ieml_rank(self):
-        return self.__rank
+def fullname(cls):
+    return cls.__module__ + '.' + cls.__qualname__
 
 
-class IEMLObjects(TreeStructure, metaclass=IEMLType):
-    closable = False
+class Singleton(type):
+    _instances = {}
 
-    def __init__(self, children, literals=None):
-        super().__init__()
-        self.children = tuple(children)
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            # this code is to clean up duplicate class if we reload modules
+            to_remove = [i for i in cls._instances if fullname(i) == fullname(cls)]
+            for i in to_remove:
+                del cls._instances[i]
 
-        _literals = []
-        if literals is not None:
-            if isinstance(literals, str):
-                _literals = [literals]
-            else:
-                try:
-                    _literals = tuple(literals)
-                except TypeError:
-                    raise InvalidIEMLObjectArgument(self.__class__, "The literals argument %s must be an iterable of "
-                                                                    "str or a str."%str(literals))
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
 
-        self.literals = tuple(_literals)
-        self._do_precompute_str()
-
-    def __gt__(self, other):
-        if not isinstance(other, IEMLObjects):
-            raise NotImplemented
-
-        if self.__class__ != other.__class__:
-            return self.__class__ > other.__class__
-
-        return self._do_gt(other)
-
-    def _do_gt(self, other):
-        return self.children > other.children
-
-    def compute_str(self, children_str):
-        return '#'.join(children_str)
-
-    def _compute_str(self):
-        if self._str is not None:
-            return self._str
-        _literals = ''
-        if self.literals:
-            _literals = '<' + '><'.join(self.literals) + '>'
-        return self.compute_str([e._compute_str() for e in self.children]) + _literals
-
-    def _do_precompute_str(self):
-        self._str = self._compute_str()
+        return cls._instances[cls]
